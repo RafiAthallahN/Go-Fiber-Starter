8f40bfc822847860ec555cdb1cb53821b13cff02
RafiAthallahN <rafiathallahnabil@gmail.com>
2024-09-03 15:56:35 +0700

    feat(utils): add function to get string time now based on format

--- backend/utils/constanta.go
+++ backend/utils/constanta.go
@@ -2,3 +2,25 @@
 This is where every const you want to define lies
 */
 package utils
+
+import "time"
+
+func GetStringTimeNow(format string) string {
+
+	now := time.Now().Local()
+
+	switch format {
+	case "date":
+		dateStr := now.Format("2006-01-02")
+		return dateStr
+	case "time":
+		timeStr := now.Format("15:04:05")
+		return timeStr
+	case "year":
+		dateStr := now.Format("2006")
+		return dateStr
+	default:
+		datetime := now.Format("2006-01-02 15:04:05")
+		return datetime
+	}
+}
