76c97e517c98f7ecf1722f122b3d5b6cdf1d78c0
RafiAthallahN <rafiathallahnabil@gmail.com>
2024-09-11 17:09:20 +0700

    refactor(config): add gorm config to set the pluralize table name by default to false

--- config/mysql.go
+++ config/mysql.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
 func MySqlConnect(env dto.MySQLEnv) *gorm.DB {
@@ -15,6 +16,9 @@ func MySqlConnect(env dto.MySQLEnv) *gorm.DB {
 		env.Username, env.Password, env.Host, env.Port, env.DB)
 
 	connect, errConnect := gorm.Open(mysql.Open(address), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: false,
+		},
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 
@@ -24,6 +28,7 @@ func MySqlConnect(env dto.MySQLEnv) *gorm.DB {
 		fmt.Println("Connect Mysql")
 	}
 
+	// Remove it if you don't want to use migration
 	if errMigrate := connect.AutoMigrate(entities.People{}); errMigrate != nil {
 		fmt.Printf("Failed to auto migrate: %v", errMigrate)
 	}
